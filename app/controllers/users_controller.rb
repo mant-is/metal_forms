b1a8e7818ebcfc2cb19ec60ce2a64701fff3bfc5
Michael Antis <antis.michael@gmail.com>
2021-02-23 14:19:16 -0500

    use vanilla forms to razzle dazzle forms
    something really goofed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
+
   def create
     # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
     @user = User.new(user_params)
+    flash.now[:alert] = "#{@user.errors.full_messages}"
+
     redirect_to new_user_path and return if @user.save
     flash.now[:alert] = "#{@user.errors.full_messages}"
     render :new
