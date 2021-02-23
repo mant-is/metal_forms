74a074d6c919ed24a371d3de7e566d1a0b99b8c9
Michael Antis <antis.michael@gmail.com>
2021-02-23 11:27:42 -0500

    create params hash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,16 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
+    @user = User.new(user_params)
     redirect_to new_user_path and return if @user.save
     flash.now[:alert] = "#{@user.errors.full_messages}"
     render :new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 
 end
