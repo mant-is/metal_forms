85a0a63c61dd812ffb19ed7f4a07cb3165539f73
Michael Antis <antis.michael@gmail.com>
2021-02-23 15:01:23 -0500

    add user validation error msgs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,7 @@ class UsersController < ApplicationController
   def create
     # @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
     @user = User.new(user_params)
-    flash.now[:alert] = "#{@user.errors.full_messages}"
-
     redirect_to new_user_path and return if @user.save
-    flash.now[:alert] = "#{@user.errors.full_messages}"
     render :new
   end
 
